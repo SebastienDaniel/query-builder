4fb92b3aef0bc6db2f528ddf80f712ab3f8a8d63
Sebastien Daniel <sebweb.ca@gmail.com>
2025-02-14 07:54:30 -0500

    feat: wrapping all components into a working prototype

---
+++ src/app/container.stories.tsx
@@ -0,0 +1,14 @@
+import { Container } from './container';
+
+export default {
+  title: 'App',
+  component: Container,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {},
+};
---
+++ src/app/container.tsx
@@ -0,0 +1,47 @@
+import debugFactory from 'debug';
+import React, { useCallback, useState } from 'react';
+import { FilterInput } from '../components/filter-input/filter-input';
+import { FilterRuleList } from '../components/filter-rule-list/filter-rule-list';
+import { FIELDS } from '../data-mocks/fields';
+import { IFilterRule } from '../types';
+import styles from './container.module.css';
+
+const debug = debugFactory('Container');
+
+export function Container() {
+  const fields = FIELDS;
+  const [filters, setFilters] = useState<IFilterRule[]>([]);
+
+  const addFilterRule = useCallback((filterRule: IFilterRule) => {
+    debug('addFilterRule', filterRule);
+    setTimeout(() => {
+      setFilters((prev) => prev.concat(filterRule));
+    }, 10);
+  }, []);
+
+  const removeFilterByIndex = useCallback((index: number) => {
+    debug('removeFilterByIndex', index);
+    setFilters((prev) => prev.filter((_, i) => i !== index));
+  }, []);
+
+  const updateFilterByIndex = useCallback((filter: IFilterRule, index: number) => {
+    debug('updateFilterByIndex', filter, index);
+    setFilters((prev) => prev.map((f, i) => (i === index ? filter : f)));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <label className={styles.container__label}>Filters</label>
+      <FilterRuleList
+        filters={filters}
+        removeFilterByIndex={removeFilterByIndex}
+        updateFilterByIndex={updateFilterByIndex}
+      />
+      <FilterInput
+        fields={fields}
+        onSubmit={addFilterRule}
+        placeholder={filters.length === 0 ? 'Add a filter...' : ''}
+      />
+    </div>
+  );
+}
